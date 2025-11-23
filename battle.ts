57a3cd53d9a0820ae822c65cf850906c0699041e
Zwynn <oneal.links@gmail.com>
2025-11-23 23:21:21 +0100

    1st draft of battle logic
    - loop until one combatant reaches zero hp
    - No UI implementation and integration of types as offensive and defensive multiplier.

--- battle.ts
+++ battle.ts
@@ -21,9 +21,9 @@ function turnOrder(a: Chromemon, b: Chromemon): Chromemon[] {
 export { turnOrder };
 
 function dmgCalc(attacker: Chromemon, defender: Chromemon, moveIndex: number) {
-    const move = attacker.moves[moveIndex];
+    const getMove = attacker.moves[moveIndex];
     let atkStat: number;    
-    if (move.category === "Phy") {
+    if (getMove.category === "Phy") {
 
         atkStat = attacker.atk;
         return; // Status-Moves verursachen keinen Schaden
@@ -31,8 +31,32 @@ function dmgCalc(attacker: Chromemon, defender: Chromemon, moveIndex: number) {
     else{
         atkStat = attacker.spAtk;
     }
-    const potDmg = move.pwr;
+    const potDmg = getMove.pwr;                                 //potenzieller Schaden
     const defStat = defender.def;
-    const dmg = Math.floor(potDmg + atkStat - defStat);
+    const dmg = Math.floor(potDmg + atkStat - defStat);         //Schaden nach Abzug der Verteidigung
     defender.hp -=  dmg > 0 ? dmg : 1;                          // Minimum Schaden ist 1
 }  
+
+function battle(monA: Chromemon, monB: Chromemon) {
+    let fighters = turnOrder(monA, monB); 
+    while (monA.hp > 0 && monB.hp > 0) {
+        dmgCalc(fighters[0], fighters[1], 0);                   //Erster Kämpfer greift an
+        alert(`${fighters[0].name}' turn.\nSelect your move!
+            ${fighters[0].moves[0].name}, ${fighters[0].moves[1].name}\n
+            ${fighters[0].moves[2].name}, ${fighters[0].moves[3].name}`);
+        prompt("");                                             //Hier soll die Move-Auswahl implementiert werden
+        if (fighters[1].hp <= 0) {
+            console.log(`${fighters[1].name} fainted! ${fighters[0].name} wins!`);
+            break;
+        }
+        dmgCalc(fighters[1], fighters[0], 0);                   //Zweiter Kämpfer greift an
+        alert(`${fighters[1].name}' turn.\nSelect your move!
+            ${fighters[1].moves[0].name}, ${fighters[1].moves[1].name}\n
+            ${fighters[1].moves[2].name}, ${fighters[1].moves[3].name}`);
+        prompt("");                                             //Hier soll die Move-Auswahl implementiert werden
+        if (fighters[0].hp <= 0) {
+            console.log(`${fighters[0].name} fainted! ${fighters[1].name} wins!`);
+            break;
+        }
+    }
+}
